18c5076f83e774c0b7cb6ccb254499ebe35e801b
alonzoshubert6991@gmail.com <shubert@zeruen.com>
2015-04-18 11:14:24 -0600

    hide

--- Game/code.js
+++ Game/code.js
@@ -1,20 +1,27 @@
 // console.log("test");
-window.onload = function() {
-	var c = document.getElementById("circle");
+// window.onload = function() {
+
+// }
+
+$(document).ready(function(){
+    // $("input#hide").click(function(){
+    //     $("#circle").hide();
+    // });
+  var c = document.getElementById("head");
 	var ctx = c.getContext("2d");
 	ctx.beginPath();
 	ctx.arc(95,50,40,0,2*Math.PI);
 	ctx.stroke();
 
-	var l = document.getElementById("line");
+	var l = document.getElementById("body");
 	var ctx1 = l.getContext("2d");
-	ctx1.moveTo(50,0);
+	ctx1.moveTo(100,0);
 	ctx1.lineTo(100,100);
 	ctx1.stroke();
-}
 
-$(document).ready(function(){
-    $("input#hide").click(function(){
-        $("#circle").hide();
-    });
+	var l = document.getElementById("rarm");
+	var ctx1 = l.getContext("2d");
+	ctx1.moveTo(0,0);
+	ctx1.lineTo(300,200);
+	ctx1.stroke();
 }); 
